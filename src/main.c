faea820413ee4e197161d65946f703476b9f9bd2
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2018-02-02 21:45:58 +0100

    Added a Sprite.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,56 @@
+#include<nes.h>
 
-extern char fastcall add(char, char);
+typedef struct _Sprite {
+    unsigned char y_position;
+    unsigned char pattern_index;
+    unsigned char attributes;
+    unsigned char x_position;
+} Sprite;
 
+Sprite *sprites = (Sprite*)0x200;
+
+#define x 128
+#define y 120
 char main() {
-    char res = add(1,2);
-    return res;
+    PPU.vram.address = 0x3F;
+    PPU.vram.address = 0x00;
+    PPU.vram.data = 0x0F;
+    PPU.vram.data = 0x0F;
+    PPU.vram.data = 0x0F;
+    PPU.vram.data = 0x0F;
+
+    PPU.vram.address = 0x3F;
+    PPU.vram.address = 0x11;
+
+    PPU.vram.data = 0x2A;
+    PPU.vram.data = 2;
+    PPU.vram.data = 0x12;
+
+    sprites[0].y_position = y-8;
+    sprites[0].x_position = x-8;
+    sprites[0].pattern_index = 0;
+    sprites[0].attributes = 0;
+
+    sprites[1].y_position = y-8;
+    sprites[1].x_position = x;
+    sprites[1].pattern_index = 0;
+    sprites[1].attributes = 0x40;
+
+    sprites[2].y_position = y;
+    sprites[2].x_position = x-8;
+    sprites[2].pattern_index = 0;
+    sprites[2].attributes = 0x80;
+
+    sprites[3].y_position = y;
+    sprites[3].x_position = x;
+    sprites[3].pattern_index = 0;
+    sprites[3].attributes = 0xC0;
+
+    SPRITE_DMA = 0x02;
+
+    PPU.control = 0x08;
+    PPU.mask = 0x18;
+
+    while(1);
+    return 0;
 }
--- src/test.c
+++
@@ -1,6 +0,0 @@
-
-char fastcall add(char a, char b);
-
-char add(char a, char b) {
-    return a+b;
-}
